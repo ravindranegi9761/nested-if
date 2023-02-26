74d4bed03211244fc2be4622946245ef85dc28df
ravindranegi9761 <125786419+ravindranegi9761@users.noreply.github.com>
2023-02-26 20:48:47 +0530

    Add files via upload

---
+++ nested if statement.py
@@ -0,0 +1,17 @@
+
+#nested if statement
+
+
+ram=32
+shyam=49
+hari=39
+if ram>shyam:
+    if ram>hari:
+        print('ram is oldest')
+else:
+    if shyam>ram:
+       if shyam>hari:
+        print('shyam is oldest') 
+if hari>shyam:
+    if hari>ram:
+       print('hari is oldesr')                             
